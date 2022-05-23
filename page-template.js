2dec339e5883e132a5663401a1bdab5beff4197b
enajera1 <eriknajera013@gmail.com>
2022-05-22 23:06:54 -0500

    test

--- page-template.js
+++ page-template.js
@@ -7,6 +7,8 @@ module.exports = templateData => {
   return `
   # ${templateData.name}             ${mit}
  
+  ## Table of Contents:
+  ### <a name="desc" href="/">Description</a> 
   ## Description:
   ${templateData.description}
 
@@ -21,7 +23,13 @@ module.exports = templateData => {
 
   ## License: 
   ${templateData.license}
+  #### Copyright ${new Date().getFullYear()} ${templateData.name}
 
+  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+  
+  The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+  
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
   ## Contribution: 
   ${templateData.contributing}
 
@@ -33,3 +41,6 @@ module.exports = templateData => {
 };
 const mit = " [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT) ";
 const bsd = " [![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause) ";
+
+const copyrightMit = `
+`
